1f0e369e51cafce4136cec2fa5410f252f1b84b5
Nag Charan <raju11297@gmail.com>
2019-09-01 23:50:56 +0530

    hkd

--- routes/Attendance.js
+++ routes/Attendance.js
@@ -1,99 +1,93 @@
-let att = require('../models/attendence.model');
-const router = require('express').Router();
-const jwt = require('jsonwebtoken');
-var lastAttDate ;
-var schedule = require('node-schedule');
- 
-var j = schedule.scheduleJob('17 * * * ', function(){
-let today = new Date;
-today = today.getDate+'-'+today.getMonth;
-console.log(today);
-  att.updateMany({$set:{
-
-  }}).then().catch()
+let att = require("../models/attendence.model");
+const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+var lastAttDate;
+var schedule = require("node-schedule");
+var j = schedule.scheduleJob("0 0 17 * * * ", function() {
+  let today = new Date();
+  let month = today.getMonth();
+  month = month + 1;
+  today = today.getDate() + "-" + month;
+  console.log(today);
+  if (lastAttDate == today) {
+    att
+      .updateMany({ $set: {} })
+      .then()
+      .catch();
+  }
 });
-router.route('/get').get((req,res)=>{
-    let token =  req.get('token');
-    if(token)
-    {
-        jwt.verify(token,'Sridevi',(err,data)=>
-        { 
-            if(!err)
-            {    att.findOne({uid:data.user_id}).then((data)=>res.send(data))
-            .catch(err => { console.log(err);res.status(404).send('User not found')});                       
-            }
-            else{
-                res.status(403).send('In valid Token')
-            }
-        });
-    }
-    else{
-        res.status(403).send('Unauthorized');
-    }
+router.route("/get").get((req, res) => {
+  let token = req.get("token");
+  if (token) {
+    jwt.verify(token, "Sridevi", (err, data) => {
+      if (!err) {
+        att
+          .findOne({ uid: data.user_id })
+          .then(data => res.send(data))
+          .catch(err => {
+            console.log(err);
+            res.status(404).send("User not found");
+          });
+      } else {
+        res.status(403).send("In valid Token");
+      }
+    });
+  } else {
+    res.status(403).send("Unauthorized");
+  }
 });
-router.route('/test').get((req,res)=>
-{
-    let today = new Date;
-    let month = today.getMonth()+1;
-    let k = String(month).padStart(2,'0')
-today = today.getDate()+'-'+k;
-var obj= new Object;
-obj[today]={  
-    am:' ',
-    pm:' '          
-}
-if(lastAttDate!==today)
-{
-    obj[today].am='H';
-    att.updateMany({$set:obj}).then().catch()
-}
-else{
-    
-}
-res.send(today)
 
-})
-router.route('/update').get((req,res)=>{
-    let date = req.get('date');
-    lastAttDate = date;
-    let uid  = req.get('uid');
-    let today = new Date;
-    console.log(today);    
-    var obj= new Object;
-        obj[date]={  
-            am:' ',
-            pm:' '          
-        }
-    if(today.getHours()>13)    
-    {    
-        att.findOne({uid:uid}).then(
-        (data)=>{
-            let am = data[date].am
-            if(am===' ')
-         {obj[date].am='A';}
-         else{
-            obj[date].am=am;
-         }
-         obj[date].pm='P';
-        } 
-     ).catch();        
-        att.updateOne({ 'uid': uid }, { $set: obj }).then(
-            res.send('updated the pm')
-        ).catch(
-           // res.send('error')
-        );       
-       
+router.route("/update").get(async (req, res) => {
+  let today = req.get("date");
+  let time = req.get("time");
+  lastAttDate = today;
+  let uid = req.get("uid");
+  let now = new Date();
+  var obj = new Object();
+  obj[today] = {
+    am: " ",
+    pm: " "
+  };
+  var am;
+  await att
+    .findOne({ uid: uid })
+    .then(data => {
+      am = data[today].am;
+      obj["total_days"] = data["total_days"]
+      console.log(am);
+    })
+    .catch();
+  if (time >= 13) {
+    if (am === " ") {
+      obj[today].am = "A";
+      obj[today].pm = "P";      
+      att
+        .updateOne({ uid: uid }, { $set: obj })
+        .then(res.send("updated the pm"))
+        .catch(error => res.send("error"));
+    } else {
+      obj[today].am = am;
+      obj[today].pm = "P";
+      obj["total_days"]=obj["total_days"]+0.5;
+      console.log(obj);
+      att
+        .updateOne({ uid: uid }, { $set: obj })
+        .then(res.send("updated the pm"))
+        .catch
+        // res.send('error')
+        ();
     }
-    else
-    {
-        obj[date].am = 'P';   
-        att.updateOne({ 'uid': uid }, { $set: obj }).then(
-            res.send('updated the am')
-        ).catch(
-           // res.send('error')
-        );
-    }
-   
- })
+  } else {
+    obj[today].am = "P";
+    obj["total_days"]=obj["total_days"]+0.5;
+    console.log(obj);
+    att
+      .updateOne({ uid: uid }, { $set: obj })
+      .then(res.send("updated the am"))
+      .catch
+      // res.send('error')
+      ();
+  }
+});
 
- module.exports = router;
+module.exports = router;
